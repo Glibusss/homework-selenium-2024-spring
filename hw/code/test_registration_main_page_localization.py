60d8cccfa7563d33b8648e6a85c22a7af4155dad
Mike WInfield <lolwut-lol@yandex.ru>
2024-05-20 05:36:09 +0300

    Registration main page test refactoring

--- hw/code/test_registration_main_page_localization.py
+++ hw/code/test_registration_main_page_localization.py
@@ -1,31 +1,15 @@
 from base_case import BaseCase
-from ui.locators.registration_main_page_locators import page_text
-
 
 class TestRegistrationMainPageLocalization(BaseCase):
     def test_main_page_russian(self, registration_main_page):
-        assert registration_main_page.text_became_visible('Русский')
-        assert registration_main_page.text_became_visible('English')
+        assert registration_main_page.language_options_became_visible()
         registration_main_page.select_language('Русский')
-        assert registration_main_page.text_became_visible(page_text['Русский']['MAIN_PAGE_TITLE'])
-        assert registration_main_page.text_became_visible(page_text['Русский']['MAIN_PAGE_SUBTITLE'])
-        assert registration_main_page.text_became_visible(page_text['Русский']['CREATE_CABINET'])
-        assert registration_main_page.text_became_visible(page_text['Русский']['IMPORT_MYTARGET'])
-        registration_main_page.hover_import_mytarget_cabinet_button_hint()
-        for item in page_text['Русский']['IMPORT_MYTARGET_HINT_ITEMS']:
-            assert registration_main_page.text_became_visible(item)
+        assert registration_main_page.language_text_became_visible('Русский')
 
     def test_main_page_english(self, registration_main_page):
-        assert registration_main_page.text_became_visible('Русский')
-        assert registration_main_page.text_became_visible('English')
+        assert registration_main_page.language_options_became_visible()
         registration_main_page.select_language('English')
-        assert registration_main_page.text_became_visible(page_text['English']['MAIN_PAGE_TITLE'])
-        assert registration_main_page.text_became_visible(page_text['English']['MAIN_PAGE_SUBTITLE'])
-        assert registration_main_page.text_became_visible(page_text['English']['CREATE_CABINET'])
-        assert registration_main_page.text_became_visible(page_text['English']['IMPORT_MYTARGET'])
-        registration_main_page.hover_import_mytarget_cabinet_button_hint()
-        for item in page_text['English']['IMPORT_MYTARGET_HINT_ITEMS']:
-            assert registration_main_page.text_became_visible(item)
+        assert registration_main_page.language_text_became_visible('English')
 
     def test_language_persists_create_cabinet_russian(self, registration_main_page):
         registration_main_page.select_language('Русский')
--- hw/code/ui/locators/registration_main_page_locators.py
+++ hw/code/ui/locators/registration_main_page_locators.py
@@ -1,33 +1,6 @@
 from selenium.webdriver.common.by import By
 from ui.locators.base_page_locators import BasePageLocators
 
-page_text = {
-    'Русский': {
-        'MAIN_PAGE_TITLE': 'Добро пожаловать\nв VK Рекламу',
-        'MAIN_PAGE_SUBTITLE': 'Перенесите настройки и кампании из существующего рекламного кабинета или создайте новый',
-        'CREATE_CABINET': 'Создать новый кабинет',
-        'IMPORT_MYTARGET': 'Использовать рекламный кабинет myTarget',
-        'IMPORT_MYTARGET_HINT_ITEMS': [
-            'Будут использованы настройки кабинета myTarget и реквизиты',
-            'Все мобильные приложения будут привязаны к новому кабинету',
-            'Вы сможете использовать аудитории myTarget в кабинете VK Рекламы',
-            'Вы сможете копировать кампании из myTarget в новый кабинет',
-        ]           
-    },
-    'English': {
-        'MAIN_PAGE_TITLE': 'Welcome \nto VK Ads',
-        'MAIN_PAGE_SUBTITLE': 'Transfer settings and campaigns from an existing account or create a new one',
-        'CREATE_CABINET': 'Create a new account',
-        'IMPORT_MYTARGET': 'Use myTarget ad account',
-        'IMPORT_MYTARGET_HINT_ITEMS': [
-            'MyTarget account settings and details will be used',
-            'All mobile applications will be linked to the new account',
-            'You will be able to use myTarget audiences in your VK Advertising account',
-            'You will be able to copy campaigns from myTarget to a new account',
-        ] 
-    },
-}
-
 class RegistrationMainPageLocators(BasePageLocators):
     @staticmethod
     def TEXT(text):
