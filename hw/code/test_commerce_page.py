6d2e2c2a6e186837346d468ba3b06e34caca9cad
Mike WInfield <lolwut-lol@yandex.ru>
2024-05-28 02:45:10 +0300

    Create catalogue test

--- hw/code/test_commerce_page.py
+++ hw/code/test_commerce_page.py
@@ -1,9 +1,7 @@
-import time
 from base_case import BaseCase
 
 
 class TestCommercePage(BaseCase):
-
     def test_is_commerce_page_opened(self, commerce_page):
         commerce_page.click_create_button()
         assert commerce_page.sidebar_became_visible()
@@ -48,3 +46,27 @@ class TestCommercePage(BaseCase):
         commerce_page.click_manual_tabs()
         assert commerce_page.manual_became_visible()
 
+    def test_create_catalogue(self, commerce_page):
+        commerce_page.click_create_button()
+        assert commerce_page.sidebar_became_visible()
+        assert commerce_page.new_catalog_has_h2_content_title()
+        assert commerce_page.name_input_became_visible()
+        assert commerce_page.has_tabs_content()
+        assert commerce_page.sidebar_buttons_became_visible()
+        assert commerce_page.cross_button_became_visible()
+        commerce_page.enter_catalogue_name()
+        commerce_page.click_feed_tabs()
+        assert commerce_page.has_feed_or_community_tabs_content()
+        commerce_page.enter_community_link()
+        commerce_page.select_4hr_period()
+        assert commerce_page.utm_checkbox_became_invisible()
+        commerce_page.click_modal_create_button()
+        assert self.is_opened(r"https://ads.vk.com/hq/ecomm/catalogs/\d+")
+        commerce_page.click_settings_button()
+        assert commerce_page.sidebar_became_visible()
+        assert commerce_page.settings_title_became_visible()
+        assert commerce_page.catalogue_name_matches_in_settings()
+        assert commerce_page.period_is_4hr_in_settings()
+        commerce_page.click_cross_button()
+        assert commerce_page.catalogue_name_matches_in_selector()
+        assert commerce_page.period_is_4hr_in_history()
