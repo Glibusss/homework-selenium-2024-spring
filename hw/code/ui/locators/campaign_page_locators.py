1dc58ebbf9f85c468c26dcb8c8972bfd236091e1
glibuss <expansifff@gmail.com>
2024-05-28 04:18:48 +0300

    selectors

--- hw/code/ui/locators/campaign_page_locators.py
+++ hw/code/ui/locators/campaign_page_locators.py
@@ -59,36 +59,86 @@ class CampaignPageLocators(BasePageLocators):
 
     BUDGET_PERIOD_SELECT = (By.XPATH, "//*[@data-testid='budget']")
 
-    AMOUNT_TIP = (By.XPATH, "//*[text()='Не менее 100&nbsp;₽']")
-
-    AMOUNT_ERROR = (By.XPATH, "//*[text()='Бюджет кампании должен быть не меньше 100₽']")
-
     START_DATE = (By.XPATH, "//*[@data-testid='start-date']")
     START_DATE_INPUT = (By.XPATH, "//*[@data-testid='start-date']//input")
-    START_DATE_CROSS = (By.XPATH, "//*[@data-testid='start-date']//*[contains(@class, 'vkuiIconButton')]")
 
     END_DATE = (By.XPATH, "//*[@data-testid='end-date']")
     END_DATE_INPUT = (By.XPATH, "//*[@data-testid='end-date']//input")
-    END_DATE_CROSS = (By.XPATH, "//*[@data-testid='end-date']//*[contains(@class, 'vkuiIconButton')]")
 
     CREATE_FOOTER = (By.XPATH, "//*[contains(@class, 'CreateFooter_footer__')]")
     CREATE_FOOTER_CONTINUE = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text() = 'Продолжить']")
-    CREATE_FOOTER_SAVE = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text() = 'Сохранить как черновик']")
-    CREATE_FOOTER_BACK = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text() = 'Назад']")
-    CREATE_FOOTER_DRAFTSTATUS = (By.XPATH, "//*[contains(@class, 'CreateFooter_draftStatus__') and text()='Изменения сохранены']")
-    CREATE_FOOTER_ERROR = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text()='1 ошибка']")
+    CREATE_FOOTER_PUBLISH = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text() = 'Опубликовать']")
 
-    DRAFTS_BUTTON = (
-    By.XPATH,
-     "//*[contains(@class, 'vkuiButton__content') and text()='Черновики']",
-    )
+    @staticmethod
+    def SECTION(name):
+        return (By.XPATH, f"//*[contains(@class, 'GroupForm_groupHeader__')]//*[text='{name}']")
+    
+    @staticmethod
+    def REGION_BUTTON(region):
+        return (By.XPATH, f"//*[contains(@class, 'vkuiButton__content') and text()='{region}']")
+    
+    @staticmethod
+    def SELECTED_REGION_CELL(region):
+        return (By.XPATH, f"//*[contains(@class, 'vkuiTypography') and text()='{region}']")
+    
+    @staticmethod
+    def GENDER_RADIO(gender):
+        return (By.XPATH, f"//*[contains(@class, 'vkuiTypography') and text()='{gender}]")
+    
+    @staticmethod
+    def AGE_SELECT_CHECK(age):
+        return (By.XPATH, f"//*[contains(@class, 'vkuiTypography') and text()='{age}']")
+    
+    PEGI_AGE_SELECT = (By.XPATH, "//*[contains(@class, 'vkuiTypography') and text()='0+']")
+
+    INTEREST_SUBSECTION = (By.XPATH, "//*[contains(@class, 'InterestsSubSection_headerText__')]")
+    INTEREST_SELECT = (By.XPATH, "//*[@data-name='interests']//input")
+
+    STOP_INTEREST_OPENER = (By.XPATH, "//*[contains(@class, 'Interests_negativeOpener__')]")
+
+    STOP_INTEREST_SELECT = (By.XPATH, "//*[contains(@class, 'vkuiFormItem') and .//*[contains(@class, 'vkuiTypography') and text()='Исключая интересы']]//input")
+
+    DESKTOP_CHECKBOX = (By.XPATH, "//*[contains(@class, 'vkuiTypography') and text()='Десктопные']")
+    MOBILE_CHECKBOX = (By.XPATH, "//*[contains(@class, 'vkuiTypography') and text()='Мобильные']")
+
+    UPLOAD_AVATAR = (By.XPATH, "//*[contains(@class, 'UploadMediaButton_buttonLogoIcon__')]")
+    TITLE_INPUT = (By.XPATH, "//input[contains(@name='заголовок, макс. 40 символов')]")
+    SHORT_DESCRIPTION = (By.XPATH, "//textarea[@name='заголовок, макс. 90 символов']")
+    LONG_DESCRIPTION = (By.XPATH, "//textarea[@name='Длинный текст для использования в лентах соцсетей (2000 знаков)']")
+    BUTTON_TEXT_INPUT = (By.XPATH, "//input[contains(@name='Доп. заголовок 30 знаков')]")
+    ADVERTISER_DESCRIPTION = (By.XPATH, "//textarea[@name='юридическая информация, макс. 115 символов']")
+
+    MEDIATEC = (By.XPATH, "//*[@data-testid='set-global-image']")
+    IMAGE_ITEM = (By.XPATH, "//*[contains(@class, 'ImageItems_imageItem__')]")
+    SUBMIT_BUTTON = (By.XPATH, "//button[@data-testid='submit']")
 
     TABLE_HEADER = (By.XPATH, "//*[contains(@class, 'BaseTable__header')]//*[@id='checkbox-all']")
+
+    DELETE_SELECT = (By.XPATH, "//*[@data-testid='select-options']")
+
+    EDIT_TAPPABLE = (By.XPATH, "//*[contains(@class, 'vkuiTypography') and text()='Редактировать']")
+
     DELETE_BUTTON = (By.XPATH, "//*[contains(@class, 'vkuiButton__content') and text()='Удалить']")
 
     DELETE_MODAL = (By.XPATH, "//*[contains(@class, 'vkuiModalPage')]")
     DELETE_MODAL_HEADER = (By.XPATH, "//*[contains(@class, 'vkuiModalPage')]//*[text()='Удалить выбранные черновики?']")
     CROSS_BUTTON = (By.XPATH, "//button[@aria-label='Close']")
 
+    @staticmethod
+    def ENTITY_NAME_CELL(name):
+        return (By.XPATH, f"//*[contains(@class, 'nameCellContent_link__') and text()='{name}']")
+    
+    PEGI_16_AGE_SELECT = (By.XPATH, "//*[contains(@class, 'vkuiTypography') and text()='16+']")
+
+    @staticmethod
+    def CHECKBOX_NAME_IS_CHECKED_OR_NOT(checkbox, state):
+        return (
+            By.XPATH,
+            f"//*[contains(@class, 'vkuiCheckbox') and .//* [contains(@class, 'vkuiTypography') and text()='{checkbox}']]//*[contains(@class, 'vkuiCheckbox__icon--{state}')]",
+        )
+    
+    AVATAR_IMAGE = (By.XPATH, "//*[contains(@class, 'AdMediaPreview_img__')]")
+
     MODAL_DELETE_BUTTON = (By.XPATH, "//*[contains(@class, 'vkuiModalPage')]//*[contains(@class, 'vkuiButton__content') and text()='Отмена']")
     MODAL_CANCEL_BUTTON = (By.XPATH, "//*[contains(@class, 'vkuiModalPage')]//*[contains(@class, 'vkuiButton__content') and text()='Удалить']")
+
